47cf18bfc912ac48bd72d1904dedfb71323db4b9
ali1129js <ali1129js@gmail.com>
2018-04-11 16:40:53 +0200

    it kinda work, socket client and server connecting

--- server.js
+++ server.js
@@ -2,32 +2,16 @@
  * @Author: Ali Ismail
  * @Date:   2018-04-11T13:12:48+02:00
  * @Last modified by:   Ali Ismail
- * @Last modified time: 2018-04-11T13:12:54+02:00
+ * @Last modified time: 2018-04-11T16:21:51+02:00
  */
-
  const express = require('express')
- const http = require('http')
- const socketIO = require('socket.io')
-
- // our localhost port
- const port = 4001
 
  const app = express()
-
- // our server instance
- const server = http.createServer(app)
-
- // This creates our socket using the instance of the server
- const io = socketIO(server)
-
- // This is what the socket.io syntax is like, we will work this later
- io.on('connection', socket => {
-   console.log('User connected')
-
-   socket.on('disconnect', () => {
-     console.log('user disconnected')
-   })
+ app.use(express.static('./socket-client/public'))
+ const port = 3000
+ const server = app.listen(port)
+ const io = require('socket.io').listen(server)
+ io.sockets.on('connection', function (socket) {
+   console.log('connected: %s',socket.id)
  })
-
- server.listen(port, () => console.log(`Listening on port ${port}`))
- view rawserver.js hosted with ❤ by GitHub
+console.log("server is running on " + port)
--- socket-client/src/App.js
+++ socket-client/src/App.js
@@ -2,15 +2,32 @@
  * @Author: Ali Ismail
  * @Date:   2018-04-11T14:13:42+02:00
  * @Last modified by:   Ali Ismail
- * @Last modified time: 2018-04-11T14:13:42+02:00
+ * @Last modified time: 2018-04-11T16:39:40+02:00
  */
 
 
 
-import React, { Component } from 'react';
+import React, { Component } from 'react'
+import io from 'socket.io-client'
 import './App.css';
 
 class App extends Component {
+  constructor(){
+    super()
+    this.state = {
+    }
+    
+  }
+  //  for emitting a socket.io event
+  componentWillMount(){
+    this.socket = io('http://localhost:3000')
+    this.socket.on('connect',this.connect)
+  }
+  connect(){
+    console.log("connected" + this.socket.id)
+  }
+
+
   render() {
     return (
       <div className="App">
